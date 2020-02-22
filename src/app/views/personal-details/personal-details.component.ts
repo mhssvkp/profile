4e0bcecc9296f2ee9cb9bbe20bed817ae64ded8d
mhssvkp <mhssvkp@gmail.com>
2020-02-22 22:59:58 +0530

    prod fixes for assets

--- src/app/views/personal-details/personal-details.component.ts
+++ src/app/views/personal-details/personal-details.component.ts
@@ -14,32 +14,32 @@ export class PersonalDetailsComponent implements OnInit {
   ];
 
   admirations:any =[
-    {name:'Suriya (Actor)',src:'/assets/images/suriya.jpg'},
-    {name:'Nayanathara (Actress)',src:'/assets/images/nayana.jpg'},
-    {name:'Ravi Teja (Actor)',src:'/assets/images/raviteja.jpg'},
-    {name:'Deepika Padukone (Actress)',src:'/assets/images/deepika.jpg'},
-    {name:'Akshay Kumar (Actor)',src:'/assets/images/akshaykumar.webp'},
-    {name:'Amish Tripathi (Author)',src:'/assets/images/amish.jpg'},
-    {name:'Robin Sharma (Author/Leadership)',src:'/assets/images/robinsharma.webp'},
-    {name:'Radhakrishnan Pillai (Author/Leadership)',src:'/assets/images/rkpillai.webp'}
+    {name:'Suriya (Actor)',src:'assets/images/suriya.jpg'},
+    {name:'Nayanathara (Actress)',src:'assets/images/nayana.jpg'},
+    {name:'Ravi Teja (Actor)',src:'assets/images/raviteja.jpg'},
+    {name:'Deepika Padukone (Actress)',src:'assets/images/deepika.jpg'},
+    {name:'Akshay Kumar (Actor)',src:'assets/images/akshaykumar.webp'},
+    {name:'Amish Tripathi (Author)',src:'assets/images/amish.jpg'},
+    {name:'Robin Sharma (Author/Leadership)',src:'assets/images/robinsharma.webp'},
+    {name:'Radhakrishnan Pillai (Author/Leadership)',src:'assets/images/rkpillai.webp'}
   ];
 
   spirutuals:any =[
-    {name:'Ramana Maharshi (Great Soul & Guru)',src:'/assets/images/ramana.jpg'},
-    {name:'Gaur Gopal Das (Author/Spirutual)',src:'/assets/images/ggdas.jpg'},
-    {name:'Sadhguru (Author/Leadership/Yoga)',src:'/assets/images/sadhguru.jpeg'},
-    {name:'Chaganti Koteswara rao (Guru/Orator)',src:'/assets/images/chaganti.jpeg'},
-    {name:'Kamakshi (Goddess)',src:'/assets/images/kamakshi.jpg'},
-    {name:'Shiva (God)',src:'/assets/images/shiva.jpg'},
-    {name:'Ganapathi (God)',src:'/assets/images/ganapathi.jpg'},
-    {name:'Rajashyamala (Goddess)',src:'/assets/images/rajashyamala.jpg'}
+    {name:'Ramana Maharshi (Great Soul & Guru)',src:'assets/images/ramana.jpg'},
+    {name:'Gaur Gopal Das (Author/Spirutual)',src:'assets/images/ggdas.jpg'},
+    {name:'Sadhguru (Author/Leadership/Yoga)',src:'assets/images/sadhguru.jpeg'},
+    {name:'Chaganti Koteswara rao (Guru/Orator)',src:'assets/images/chaganti.jpeg'},
+    {name:'Kamakshi (Goddess)',src:'assets/images/kamakshi.jpg'},
+    {name:'Shiva (God)',src:'assets/images/shiva.jpg'},
+    {name:'Ganapathi (God)',src:'assets/images/ganapathi.jpg'},
+    {name:'Rajashyamala (Goddess)',src:'assets/images/rajashyamala.jpg'}
   ];
 
   hobbies:any=[
-    {name:'Reading Books',src:'/assets/images/books.svg'},
-    {name:'Listening to Music',src:'/assets/images/music.png'},
-    {name:'Watching Movies',src:'/assets/images/movies.png'},
-    {name:'Petting Animals',src:'/assets/images/petting.png'}
+    {name:'Reading Books',src:'assets/images/books.svg'},
+    {name:'Listening to Music',src:'assets/images/music.png'},
+    {name:'Watching Movies',src:'assets/images/movies.png'},
+    {name:'Petting Animals',src:'assets/images/petting.png'}
   ]
 
   constructor() { }
--- src/app/views/tech-profiles/tech-profiles.component.ts
+++ src/app/views/tech-profiles/tech-profiles.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class TechProfilesComponent implements OnInit {
 
   sprofiles:any = [
-    {label:'Facebook',src:'/assets/images/fb.webp',link:'https://www.facebook.com/kvatppavan'},
-    {label:'Instagram',src:'/assets/images/instagram.svg',link:'https://www.instagram.com/vayu_puthra/'},
+    {label:'Facebook',src:'assets/images/fb.webp',link:'https://www.facebook.com/kvatppavan'},
+    {label:'Instagram',src:'assets/images/instagram.svg',link:'https://www.instagram.com/vayu_puthra/'},
   ]
 
   techProfiles:any=[
-    {label:'LinkedIn',src:'/assets/images/linkedin.png',link:'https://www.linkedin.com/in/mhssvkp/'},
-    {label:'Github',src:'/assets/images/github.png',link:'https://github.com/mhssvkp'},
-    {label:'Stackoverflow',src:'/assets/images/stackoverflow.png',link:'https://stackoverflow.com/users/11751221/pavan-kumar'}
+    {label:'LinkedIn',src:'assets/images/linkedin.png',link:'https://www.linkedin.com/in/mhssvkp/'},
+    {label:'Github',src:'assets/images/github.png',link:'https://github.com/mhssvkp'},
+    {label:'Stackoverflow',src:'assets/images/stackoverflow.png',link:'https://stackoverflow.com/users/11751221/pavan-kumar'}
   ]
 
   constructor() { }
